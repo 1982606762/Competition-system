372338f89c6b7264466e5d21e2dcf235bb4e8048
Mr.Z <1982606762@qq.com>
2022-03-28 10:35:34 +0800

    Initial Commit

---
+++ Competition system/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Competition system
+//
+//  Created by XuanLang Z on 2022/3/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
