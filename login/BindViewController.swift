d87bffc6badf10ad6e2544e67420da5e0d828459
Mr.Z <1982606762@qq.com>
2022-03-28 19:08:38 +0800

    add login view

---
+++ login/BindViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BindViewController.swift
+//  Competition system
+//
+//  Created by XuanLang Z on 2022/3/28.
+//
+
+import Foundation
---
+++ login/RegisterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  registerViewController.swift
+//  Competition system
+//
+//  Created by XuanLang Z on 2022/3/28.
+//
+
+import Foundation
